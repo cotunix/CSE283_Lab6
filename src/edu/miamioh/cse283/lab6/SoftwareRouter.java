c33c14a4a133bd31d41dabb2e38d3d34d3fb8792
richarkc <richarkc@miamioh.edu>
2014-10-21 17:17:46 -0400

    Created pair class for storing subnet mask, debugging

--- src/edu/miamioh/cse283/lab6/SoftwareRouter.java
+++ src/edu/miamioh/cse283/lab6/SoftwareRouter.java
@@ -1,8 +1,8 @@
 package edu.miamioh.cse283.lab6;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map.Entry;
+
+
 
 /**
  * Software router template for CSE283 Lab 6, FS2014.
@@ -11,7 +11,19 @@ import java.util.Map.Entry;
  */
 public class SoftwareRouter {
 
-	protected HashMap<Integer, Link> h = new HashMap<Integer, Link>();
+	public class Pair<A, B> {
+		public A K;
+		public B T;
+		public Pair(A K, B T) {
+			this.K = K;
+			this.T = T;
+		}
+		public Pair() {
+			this.K = null;
+			this.T = null;
+		}
+	}
+	protected HashMap<Integer, Pair<Link, Integer>> h = new HashMap<Integer, Pair<Link, Integer>>();
 
 	/**
 	 * Adds the given link and (network, subnet mask) to this router's routing
@@ -25,8 +37,8 @@ public class SoftwareRouter {
 	 *            is the number of bits for the network prefix.
 	 */
 	public void addLink(Link link, Address network_address, int subnet_mask) {
-		int subnet = ~((int)(Math.pow(2, 32 - subnet_mask) - 1));
-		h.put(network_address.ip & subnet, link);
+		int subnet = ~((int) (Math.pow(2, 32 - subnet_mask) - 1));
+		h.put(network_address.ip & subnet, new Pair<Link, Integer>(link, subnet_mask));
 	}
 
 	/**
@@ -37,15 +49,9 @@ public class SoftwareRouter {
 	 */
 	public void removeLink(Link link) {
 
-		// God bless stackoverflow
-		Iterator<Entry<Integer, Link>> i = h.entrySet().iterator();
-		while (i.hasNext()) {
-			Entry<Integer, Link> hEntry = i.next();
-			if (hEntry.getValue() == link) {
-				i.remove();
-			}
 
-		}
+		h.values().remove(link);
+		
 	}
 
 	/**
@@ -58,10 +64,13 @@ public class SoftwareRouter {
 	public void receivePacket(Packet pkt) {
 		// once the correct outgoing link has been identified, call
 		// link.send(pkt, this);
+		Pair<Link, Integer> temp = new Pair<Link, Integer>();
 		for (int i = 0; i <= 32; i++) {
-
-			if (h.get(pkt.dst.getIP() & ~((int)(Math.pow(2, i) - 1))) != null) {
-				h.get(pkt.dst.getIP() & ~((int)(Math.pow(2, i) - 1))).send(pkt);
+			temp = h.get(pkt.dst.getIP() & ~((int) (Math.pow(2, i) - 1)));
+			if (temp != null) {
+				if (temp.T == (32 - i))
+					temp.K.send(pkt);
+						
 				break;
 			}
 
